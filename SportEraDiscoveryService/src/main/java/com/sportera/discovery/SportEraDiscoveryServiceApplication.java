dee9f0acdea3d3732c67f51c954823ee22e684fa
Andrew <andrewriznykua93@gmail.com>
2020-11-23 16:35:40 -0600

    discovery service setup

---
+++ SportEraDiscoveryService/src/main/java/com/sportera/discovery/SportEraDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sportera.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportEraDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportEraDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ SportEraDiscoveryService/src/test/java/com/sportera/discovery/SportEraDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportera.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportEraDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
